fc2f11dcf885227e7c1017a957f18bcaa0dd01eb
Jacob Peoples <jacobjpeoples@gmail.com>
2019-04-25 11:39:24 -0400

    Update jssg, use new conf, fix added newlines in blog index

--- build.py
+++ build.py
@@ -2,6 +2,25 @@ import sys
 
 import jssg
 
+def compute_href(env, render_context, inpath, outpath, rel_name="href", full_name="fullhref"):
+    href = outpath.relative_to(env['output_directory']).as_posix()
+    additional_ctx = {
+        'href': href,
+        'fullhref': render_context['base_url'] + href
+    }
+    return additional_ctx
+
+def add_push_to_collection(env, render_context, inpath, outpath, page_collection):
+    additional_info = {
+        "href": render_context['href'],
+        'fullhref': render_context['fullhref']
+    }
+    additional_ctx = {
+        'push_to_collection': lambda x: jssg.push_page(page_collection, x, additional_info)
+    }
+
+    return additional_ctx
+
 if __name__ == "__main__":
     # set base url
     base_url = 'https://jpeoples.github.io/'
@@ -9,44 +28,40 @@ if __name__ == "__main__":
         base_url = 'http://localhost:8080/'
         print(base_url)
 
+    conf = dict(
+        input_directory="src",
+        output_directory="build",
+        jinja=dict(
+            load_paths_with_prefix=(('layouts', 'layouts'),),
+            base_render_context=dict(
+                css="site.css",
+                base_url=base_url
+            ),
+            jit_context=[compute_href]
+        ),
+        markdown_filter={},
+        date_filters={}
+    )
 
+    env = jssg.Environment(conf)
 
-    source_dir = 'src'
-    build_dir = 'build'
+    posts=[]
+    jinja_file = env.file.jinja.add_jit_context(add_push_to_collection, page_collection=posts)
 
-    rule_env = jssg.RuleEnv(source_dir, build_dir)
-    jenv = jssg.jinja_env(load_paths_with_prefix=(('layouts','layouts'),))
-
-    # additional template render context
-    render_env = {
-            'css': "site.css",
-            'base_url': base_url
-            }
-
-    jinja_file_t = jssg.JinjaFile(jenv, base_url, build_dir, render_ctx=render_env)
-
-    # Build the blog, storing the posts in a PageCollection
-    posts = []
-    jinja_file = jinja_file_t.override(page_collection=posts)
-    rule_env.build_dir([
-        # ignore drafts and index pages
-        (('*.draft.*', '*index.jinja.*', '*.xml', '*.swp'), jssg.ignore_file),
+    env.build_dir([
+        (('*.draft.*', '*index.jinja.*', '*.xml', '*.swp'), env.rule.ignore),
         # process posts
-        (('*.jinja.md', '*.jinja.html'), (jssg.to_html, jinja_file)),
+        (('*.jinja.md', '*.jinja.html'), (env.path.to_html, jinja_file)),
         # copy everything else
-        ('*', (jssg.mirror, jssg.copy_file))
+        ('*', env.rule.copy)
         ], subdir='blog')
 
     posts = jssg.sort_pages(posts)
-
-    # Build the rest of the site, including the rss and index for the
-    # blog (which rely on posts)
-    jinja_file = jinja_file_t.override(additional_ctx={'posts': posts})
-    rule_env.build_dir([
-        (('blog/index.jinja.html', 'blog/rss.jinja.xml'), (jssg.remove_internal_extensions, jinja_file)),
-        (('blog/*', '*.swp'), jssg.ignore_file),
-        ('*.jinja.md', (jssg.to_html, jinja_file)),
-        ('*.jinja.*', (jssg.remove_internal_extensions, jinja_file)),
-        ('*', (jssg.mirror, jssg.copy_file))
+    jinja_file = env.file.jinja.add_context({'posts': posts})
+    env.build_dir([
+        (('blog/index.jinja.html', 'blog/rss.jinja.xml'), (env.path.remove_internal_extensions, jinja_file)),
+        (('blog/*', '*.swp'), env.rule.ignore),
+        ('*.jinja.md', (env.path.to_html, jinja_file)),
+        ('*.jinja.*', (env.path.remove_internal_extensions, jinja_file)),
+        ('*', (env.rule.copy))
         ])
-
